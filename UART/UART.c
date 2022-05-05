e987f6dd91155b4a614a6511ae67ce3b967d2486
JustOnlyTim <madshassestavnsbo@googlemail.com>
2022-05-05 12:44:19 +0200

    UART
    laver c og h filer

---
+++ UART/UART.c
@@ -0,0 +1,17 @@
+
+void handleByteReceived(int byteReceived)
+{
+    switch(byteReceived)
+    {
+        case 'x' :
+        {
+            
+        }
+        break;
+        default :
+        {
+
+        }
+        break;
+    }
+}
