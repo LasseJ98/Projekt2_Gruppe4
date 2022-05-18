8e77b70720794c0872d6adfaf782dfbcd49bd261
ThorSporring <90454987+ThorSporring@users.noreply.github.com>
2022-05-18 20:53:27 +0200

    Led lort fuck dig

--- Reciever_module/main.cpp
+++ Reciever_module/main.cpp
@@ -2,21 +2,29 @@
 #include "led.h"
 #include "X10_Modtager.h"
 
+#define RX_PIN 18
+#define ZERO_PIN 4
+#define LED_OUT 9
+
 Led led;
 X10_Modtager HouseA;
+byte unitId = 0b01101001;
+unsigned int currentFunction;
 
 void setup() {
   // put your setup code here, to run once:
-  
-  led.init();
+  HouseA.initX10_modtager(RX_PIN,ZERO_PIN);
+  led.init(LED_OUT);
+
 }
 
 void loop() {
   
-  led.setPWM(100);
-  delay(500);
-  led.setPWM(50);
-  delay(500);
-  led.setPWM(10);
-  delay(500);
+  int *commands = HouseA.receiveCommands();
+
+  if(commands[0] == unitId)
+  {
+    analogWrite(LED_OUT, HIGH);
+  }
+
 }
