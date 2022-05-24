a44daffa66bf13da7f472dc23bf1db9530885b21
JustOnlyTim <madshassestavnsbo@googlemail.com>
2022-05-24 09:36:32 +0200

    Interrupts v1
    attempt one to test interrupts

--- NyArduino/src/main.cpp
+++ NyArduino/src/main.cpp
@@ -4,7 +4,7 @@ void setup()
 {
   // open the serial port:
   Serial.begin(9600);
-
+  activate();
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(8, INPUT);
   // initialize control over the keyboard:
@@ -14,60 +14,60 @@ void setup()
 
 void loop()
 {
-  bool AdminAdgang = digitalRead(8); // Aflæs fra DE2 boardet hvis systemet skal være låst op eller ej
-  if (AdminAdgang == true) // Hvis der er låst op for adming adgang
-  {
-  if (Serial.available() > 0) // Tjek for indkommende seriel data
-  {
-    char inChar = Serial.read(); // Læs indkommende seriel data
-    Serial.println(inChar); // Udskriv hvad brugeren indtaster
+  //bool AdminAdgang = digitalRead(8); // Aflæs fra DE2 boardet hvis systemet skal være låst op eller ej
+ // if (AdminAdgang == true) // Hvis der er låst op for adming adgang
+ // {
+ // if (Serial.available() > 0) // Tjek for indkommende seriel data
+  //{
+  //  char inChar = Serial.read(); // Læs indkommende seriel data
+ //   Serial.println(inChar); // Udskriv hvad brugeren indtaster
 
-    if (inChar == '1')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at konfigurere morgenrutine
-    }
-    else if (inChar == '2')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at konfigurere aftenrutine
+ //   if (inChar == '1')
+ //   {
+  //    digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at konfigurere morgenrutine
+    //}
+    //else if (inChar == '2')
+   // {
+  //    digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at konfigurere aftenrutine
 
-    }
-    else if (inChar == '3')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at afbryde admin adgang
-    }
-    else
-    {
-      Serial.print("Du har ikke valgt en gyldig valgmulighed");
-    }
+  //  }
+ //   else if (inChar == '3')
+//    {
+//      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at afbryde admin adgang
+//    }
+//     else
+//     {
+//       Serial.print("Du har ikke valgt en gyldig valgmulighed");
+//     }
 
-    if (inChar == '1') // Når enheden er låst for admin adgang. Manuelt styring
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre lys
-    }
-    else if (inChar == '2')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre lyd
-    }
-    else if (inChar == '3')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre dørlås
-    }
-    else if (inChar == '4')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre gardiner
-    }
-    else if (inChar == '5')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre TV
-    }
-    else if (inChar == '6')
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre vandbeholder
-    }
-    else
-    {
-      Serial.print("Du har ikke valgt en gyldig valgmulighed");
-    }
-  }
-  }
-}
+//     if (inChar == '1') // Når enheden er låst for admin adgang. Manuelt styring
+//     {
+//       digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre lys
+//     }
+//     else if (inChar == '2')
+//     {
+//       digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre lyd
+//     }
+//     else if (inChar == '3')
+//     {
+//       digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre dørlås
+//     }
+//     else if (inChar == '4')
+//     {
+//       digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre gardiner
+//     }
+//     else if (inChar == '5')
+//     {
+//       digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre TV
+//     }
+//     else if (inChar == '6')
+//     {
+//       digitalWrite(LED_BUILTIN, HIGH); // Skal erstattes med kode for at styre vandbeholder
+//     }
+//     else
+//     {
+//       Serial.print("Du har ikke valgt en gyldig valgmulighed");
+//     }
+//   }
+//   }
+ }
